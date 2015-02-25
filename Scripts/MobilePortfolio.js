a3789573617c84170a2d7928029fe50951cbc2dd
kawserfatema <kawserfatema@yahoo.com>
2015-02-25 11:06:52 -0800

    commit4
    commit4

---
+++ Scripts/MobilePortfolio.js
@@ -0,0 +1,9 @@
+﻿function button_clicked() {
+    var txt = " You selected : " + $('input:radio[name=radio-choice-1]:checked').val();
+    $('#popupDialog').text(txt);
+    $('#popupDialog').popup('open');
+}
+
+function write_xml() {
+    
+}
